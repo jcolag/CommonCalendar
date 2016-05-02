96ae719bfbdea066044dde1b51afa947b47a7ec6
John Colagioia <john@colagioia.net>
2016-05-02 07:02:32 -0400

    Proof of concept month

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let dow = ["Duinday", "Sitaday", "Wikiday", "Tuxday", "Gnuday", "Commonday"];
+    let moy = ["Jabim", "Zodrak", "Trogool", "Yanar", "Shkumbe", "Habniah", "Skarl", "Mikon", "Pertunda", "Kib", "Broket"];
+    let yoff = 0;
+    let dpm = 33;
+    let dpw = dow.len();
+    let mpy = moy.len();
+    
+    for dd in 0..dpw {
+        let mut abbrd = String::from(dow[dd]);
+        abbrd.truncate(2);
+        print!("{} ", abbrd);
+    }
+    
+    for day in 1 .. dpm + 1 {
+        if (day + yoff) % dpw == 1 {
+            println!("");
+        }
+
+        print!("{:2} ", day);
+    }
+
+    println!("");
 }
