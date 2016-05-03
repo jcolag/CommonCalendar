0e9a53b39077ecf72f8deea07c91f0ecca30ca5e
John Colagioia <john@colagioia.net>
2016-05-03 07:22:36 -0400

    Use offset

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,24 @@ fn main() {
     let dpm = 33;
     let dpw = dow.len();
     let mpy = moy.len();
-    
+    let mut moff = yoff;
+
     for dd in 0..dpw {
         let mut abbrd = String::from(dow[dd]);
         abbrd.truncate(2);
         print!("{} ", abbrd);
     }
     
+    if moff != 0 {
+        println!("");
+    }
+    
+    for _ in 0 .. moff {
+        print!("   ");
+    }
+    
     for day in 1 .. dpm + 1 {
-        if (day + yoff) % dpw == 1 {
+        if (day + moff) % dpw == 1 {
             println!("");
         }
 
@@ -21,4 +30,5 @@ fn main() {
     }
 
     println!("");
+    moff = if moff == 0 { 3 } else { 0 };
 }
