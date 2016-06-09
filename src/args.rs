0e4ed3bf446b326210faedb6a7bfbe5476f3ac70
John Colagioia <john@colagioia.net>
2016-06-09 05:54:56 -0400

    Stub out parameter list

--- src/args.rs
+++ src/args.rs
@@ -3,6 +3,9 @@ use std::env::Args;
 
 pub struct Arguments {
     pub program_name: String,
+    pub month: usize,
+    pub year: usize,
+    pub date_only: bool,
     pub exit: bool,
 }
 
@@ -14,6 +17,9 @@ fn print_usage(program: &str, opts: Options) {
 pub fn parse_args(arguments: Args) -> Arguments {
     let mut opts = Options::new();
     opts.optflag("h", "help", "print this help menu");
+    opts.optflag("d", "date", "only print today's date");
+    opts.optopt("y", "year", "only print the year's calendar", "YEAR");
+    opts.optopt("m", "month", "only print the month's calendar", "MONTH");
 
     let matches = match opts.parse(arguments) {
         Ok(m) => { m }
@@ -22,6 +28,9 @@ pub fn parse_args(arguments: Args) -> Arguments {
 
     let mut args = Arguments {
         program_name: "".to_string(),
+        date_only: matches.opt_present("d"),
+        month: 0,
+        year: 0,
         exit: matches.opt_present("h"),
     };
 
