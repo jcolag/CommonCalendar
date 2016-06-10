a22420300fccb56ae875f96a85bf6afb5dc97d24
John Colagioia <john@colagioia.net>
2016-06-10 06:28:27 -0400

    Parse new arguments

--- src/args.rs
+++ src/args.rs
@@ -3,7 +3,9 @@ use std::env::Args;
 
 pub struct Arguments {
     pub program_name: String,
+    pub has_month: bool,
     pub month: usize,
+    pub has_year: bool,
     pub year: usize,
     pub date_only: bool,
     pub exit: bool,
@@ -29,8 +31,22 @@ pub fn parse_args(arguments: Args) -> Arguments {
     let mut args = Arguments {
         program_name: "".to_string(),
         date_only: matches.opt_present("d"),
-        month: 0,
-        year: 0,
+        has_month: matches.opt_present("m"),
+        month: match matches.opt_str("m") {
+            Some(m) => match m.parse::<usize>() {
+                Ok(mm) => mm,
+                Err(_) => 0,
+            },
+            None => 0,
+        },
+        has_year: matches.opt_present("y"),
+        year: match matches.opt_str("y") {
+            Some(y) => match y.parse::<usize>() {
+                Ok(mm) => mm,
+                Err(_) => 0,
+            },
+            None => 0,
+        },
         exit: matches.opt_present("h"),
     };
 
